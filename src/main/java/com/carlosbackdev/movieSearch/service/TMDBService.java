570d4305192ea2a9fc54b367ab7b2d79685339d9
carlos <carloslospinos@gmail.com>
2025-01-24 03:24:37 +0100

    consulta con titulo traducido y pagina aleatoria

--- src/main/java/com/carlosbackdev/movieSearch/service/TMDBService.java
+++ src/main/java/com/carlosbackdev/movieSearch/service/TMDBService.java
@@ -109,35 +109,111 @@ public class TMDBService {
         return keywordsIds;
     }
 
+public Object fetchMovies(String phrase, List<String> genreIds, List<String> properNames, List<Integer> year, List<String> keywords, List<String> country) {
+    OkHttpClient client = new OkHttpClient();
+    String query = QueryBuilder.buildTMDBQuery(phrase, genreIds, properNames, year, API_KEY, keywords, country);
+    int totalPages = 0;
+    int pageNumber = 1;
+    String url = API_URL + query + "&page=" + pageNumber;
+    System.out.println("Consulta final: " + url);
+    System.out.println("Consulta final con palabras clave: " + keywords);
 
-    public Object fetchMovies(String phrase, List<String> genreIds, List<String> properNames, List<Integer> year,List<String> keywords,List<String> country) {
-        OkHttpClient client = new OkHttpClient();
-        String query = QueryBuilder.buildTMDBQuery(phrase,genreIds, properNames, year,API_KEY,keywords,country);
-        String url = API_URL + query;
-        System.out.println("consulta final" + url);
-        System.out.println("consulta final" + keywords);
+    Request request = new Request.Builder()
+        .url(url)
+        .get()
+        .addHeader("Accept", "application/json")
+        .addHeader("Authorization", "Bearer " + API_KEY)
+        .build();
 
-        Request request = new Request.Builder()
-            .url(url)
-            .get()
-            .addHeader("Accept", "application/json") 
-            .addHeader("Authorization", "Bearer " + API_KEY)
-            .build();
+    try (Response response = client.newCall(request).execute()) {
+        if (response.isSuccessful()) {
+            String responseBody = response.body().string();
+            ObjectMapper objectMapper = new ObjectMapper();
 
-        try (Response response = client.newCall(request).execute()) {
-            if (response.isSuccessful()) {
-                String responseBody = response.body().string();  
-                ObjectMapper objectMapper = new ObjectMapper();
-                
-                return objectMapper.readValue(responseBody, Object.class);
-            } else {
+            Map<String, Object> resultMap = objectMapper.readValue(responseBody, Map.class);
+            totalPages = (int) resultMap.get("total_pages");
 
-                throw new RuntimeException("Error al consultar la API de TMDB");
+            // Ahora, hacemos una consulta con una página aleatoria
+            if(totalPages>500){
+                totalPages=500;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error al consultar la API de TMDB", e);
+            int randomPage = (int) (Math.random() * totalPages) + 1; 
+            String randomPageUrl = API_URL + query + "&page=" + randomPage;
+            System.out.println("Consulta con página aleatoria: " + randomPageUrl);
+
+            Request randomPageRequest = new Request.Builder()
+                .url(randomPageUrl)
+                .get()
+                .addHeader("Accept", "application/json")
+                .addHeader("Authorization", "Bearer " + API_KEY)
+                .build();
+
+            try (Response randomPageResponse = client.newCall(randomPageRequest).execute()) {
+                if (randomPageResponse.isSuccessful()) {
+                    String randomPageResponseBody = randomPageResponse.body().string();
+                    Map<String, Object> randomPageResultMap = objectMapper.readValue(randomPageResponseBody, Map.class);
+
+                    List<Map<String, Object>> randomPageResults = (List<Map<String, Object>>) randomPageResultMap.get("results");
+
+                    for (Map<String, Object> item : randomPageResults) {
+                        Integer movieId = (Integer) item.get("id");
+                        String mediaType = (String) item.get("media_type");
+
+                        String randomPageTranslateUrl = API_URL + mediaType + "/" + movieId + "/translations?api_key=" + API_KEY;
+                        Request randomPageTranslateRequest = new Request.Builder()
+                            .url(randomPageTranslateUrl)
+                            .get()
+                            .addHeader("Accept", "application/json")
+                            .build();
+
+                        try (Response randomPageTranslateResponse = client.newCall(randomPageTranslateRequest).execute()) {
+                            if (randomPageTranslateResponse.isSuccessful()) {
+                                String randomPageTranslateResponseBody = randomPageTranslateResponse.body().string();
+                                Map<String, Object> translateData = objectMapper.readValue(randomPageTranslateResponseBody, Map.class);
+                                List<Map<String, Object>> translations = (List<Map<String, Object>>) translateData.get("translations");
+
+                                String translatedTitle = null;
+                                for (Map<String, Object> translation : translations) {
+                                    String iso = (String) translation.get("iso_639_1");
+                                    String countr = (String) translation.get("iso_3166_1");
+
+                                    if ("es".equals(iso) && translatedTitle == null) {
+                                        Map<String, String> data = (Map<String, String>) translation.get("data");
+                                        translatedTitle = data.get("title");
+                                    }
+
+                                    if ("es".equals(iso) && "MX".equals(country) && translatedTitle == null) {
+                                        Map<String, String> data = (Map<String, String>) translation.get("data");
+                                        translatedTitle = data.get("title");
+                                    }
+                                }
+                                
+                                if (translatedTitle != null) {
+                                    item.put("title", translatedTitle);
+                                }
+                            }
+                        }
+                    }
+
+             
+                    return randomPageResultMap;
+                    
+                    
+                } else {
+                    throw new RuntimeException("Error al consultar la API de TMDB con la página aleatoria");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error al consultar la API de TMDB con la página aleatoria", e);
+            }
+        } else {
+            throw new RuntimeException("Error al consultar la API de TMDB");
         }
-                
-  }
+    } catch (IOException e) {
+        e.printStackTrace();
+        throw new RuntimeException("Error al consultar la API de TMDB", e);
+    }
+}
+
+
 }
