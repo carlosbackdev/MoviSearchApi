3db2a08e29e5717b7d77cba45915189619a06578
carlos <carloslospinos@gmail.com>
2025-03-04 23:32:05 +0100

    keyword

--- src/main/java/com/carlosbackdev/movieSearch/text/KeywordExtractor.java
+++ src/main/java/com/carlosbackdev/movieSearch/text/KeywordExtractor.java
@@ -25,6 +25,7 @@ public class KeywordExtractor {
         "them","say","give","cacth","will","myself","something","anything","some","someone","lot",
         "ecommend","ecommends","feel","today","tomorrow","tell","tells","says","give",
         "not","tvs","film","films","shows","show","pass","today","watch","watchs","off","says","why","yes"
+        "see","seen","saw"
     ));
 
     public List<String> extractKeywords(String phrase) {
